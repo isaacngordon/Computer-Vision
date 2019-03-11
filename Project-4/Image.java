38d4a71eba61568faff5fae7dfd20ff77a0ddfa7
Isaac Gordon <tzachi.gordon@gmail.com>
2019-03-10 21:22:02 -0400

    Additional Image functions added

--- Project-4/Image.java
+++ Project-4/Image.java
@@ -2,12 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Image {
-    int numRows;
-    int numCols;
-    int minVal;
-    int maxVal;
+    int numRows;                //number of rows based on header
+    int numCols;                //number of cols based on header
+    int minVal;                 //lowest value based on header
+    int maxVal;                 //largest value based on header
 
-    int[][] imgAry;
+    int[][] imgAry;             //actual image data
+    boolean isFramed;           //is this a framed image?
 
     public Image(){
 
@@ -31,6 +32,9 @@ public class Image {
             }//for
         }//for
         inStream.close();
+
+        //set as unframed image
+        isFramed = false;
     }//constrcutor from file
 
     public Image(int numRows, int numCols, int minVal, int maxVal){
@@ -41,6 +45,9 @@ public class Image {
         initWithZeros();
     }//constructor w header
 
+    /**
+     * Initializs imgAry with a 2D array of zeros.
+     */
     public void initWithZeros(){
         imgAry = new int[numRows][numCols];
         for(int r = 0; r < numRows; r++){
@@ -49,6 +56,77 @@ public class Image {
             }//for
         }//for
     }//initWithZeroes
+
+    /**
+     * @param imgAry the imgAry to set
+     */
+    public void copyImage(Image image) {
+        this.imgAry = image.getImgAry();
+        this.numCols = image.getNumCols();
+        this.numRows = image.getNumRows();
+        this.minVal = image.getMinVal();
+        this.maxVal = image.getMaxVal();
+    }//copyImage
+
+    /**
+     * Creates a frame around this image based of the origin of the structuring element
+     * @param structuringElement
+     * @return a copy of this image framed
+     */
+    public Image frame(StructuringElement structuringElement){
+        Image framedImage = new Image();
+
+        //get all frame dims
+        int[] frameDims = computeFrame(structuringElement);
+        int top = frameDims[0];
+        int bottom = frameDims[1];
+        int left  = frameDims[2];
+        int right = frameDims[3];
+
+        //TODO:frame the current image and return a framed image
+
+        framedImage.setFramed(true);
+        return framedImage;
+    }//frame()
+
+    /**
+     * Computes the framing dimesntions based on a given structuring element.
+     * @return int[4] = [topFrameDim, bottomFrameDim, leftFrameDim, rightFrameDim]
+     */
+    private int[] computeFrame(StructuringElement structuringElement){
+        int[] dims = new int[4];
+        //TODO: find distance from origin to all sides, and add said dims to dims[]
+
+        return dims;
+    }//computeFrame
+
+    /**
+     * Unframe an image and return the unframed image
+     * @return
+     */
+    public Image unframe(Image framedImage, StructuringElement structuringElement){
+        Image unframedImage = new Image();
+        int[] dims = computeFrame(structuringElement);
+
+        //TODO: unframe image
+        unframedImage.setFramed(false);
+        return unframedImage;
+    }//unframe
+
+    /**
+     * Prints the image to the console
+     */
+    public void prettyprint(){
+
+    }//prettyprint to console
+
+    /**
+     * Prints the image to an outputfile
+     * @param outputFile the file to send the image
+     */
+    public void prettyprint(File outputFile){
+
+    }//preetyprint to outputFile
 
     /**
      * @return the numRows
@@ -116,14 +194,21 @@ public class Image {
     /**
      * @param imgAry the imgAry to set
      */
-    public void copyImage(Image image) {
-        this.imgAry = image.getImgAry();
-        this.numCols = image.getNumCols();
-        this.numRows = image.getNumRows();
-        this.minVal = image.getMinVal();
-        this.maxVal = image.getMaxVal();
-    }//copyImage
+    public void setImgAry(int[][] imgAry) {
+        this.imgAry = imgAry;
+    }
 
-    
+    /**
+     * @return the isFramed
+     */
+    public boolean isFramed() {
+        return isFramed;
+    }
 
+    /**
+     * @param isFramed the isFramed to set
+     */
+    public void setFramed(boolean isFramed) {
+        this.isFramed = isFramed;
+    }
 }//class
